689e44e180e7c8a661091b59db731bfcb099a04b
Adejumo Ridwan Suleiman <adejumo999@gmail.com>
2023-11-19 23:05:30 +0100

    add

--- DCTA/src/routes/create_did/+page.svelte
+++ DCTA/src/routes/create_did/+page.svelte
@@ -1,9 +1,33 @@
-<!-- src/routes/yourPage.svelte -->
+<script>
+  import { webcrypto } from "node:crypto";
+  import { onMount } from "svelte";
 
-<script context="module" lang="ts">
-    import { load } from 'src/routes/+page.server.js';
-  
-    export const loadContext = load;
-  </script>
-  
-<a>{myDID}</a>
+  if (!globalThis.crypto) {
+    globalThis.crypto = webcrypto;
+  }
+
+  import { Web5 } from "@web5/api";
+
+  let userDid;
+
+  onMount(async () => {
+    const { web5, did: userDid } = await Web5.connect();
+  });
+
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(userDID);
+      alert("DID copied to clipboard!");
+    } catch (err) {
+      console.error("Unable to copy DID to clipboard", err);
+    }
+  };
+</script>
+
+<main>
+  <h1>Your User DID</h1>
+  {#if userDid}
+    <p>{userDid}</p>
+    <button on:click={copyToClipboard}>Copy DID</button>
+  {/if}
+</main>
